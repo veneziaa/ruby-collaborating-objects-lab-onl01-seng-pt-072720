fbc6ee432fdb96133f40c7a43f195c4f0ef05abe
Venice Vargas <venicevargas1@live.com>
2020-09-19 08:22:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,21 @@
 class Song
-  attr_accessor :name, :artist
+  attr_accessor :artist, :name
 
   def initialize(name)
     @name = name
   end
 
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
 
-  def artist_name=(name_string)
-      song_artist = Artist.find_or_create_by_name(name_string)
-       @artist = song_artist
-      @artist.add_song(self)
+  def self.new_by_filename(file)
+    song_info = file.chomp(".mp3").split(" - ")
+    song = Song.new(song_info[1])
+    song.artist_name = song_info[0]
+    song
   end
 end
+
+
