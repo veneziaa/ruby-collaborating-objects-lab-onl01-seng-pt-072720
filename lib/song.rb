b79816ca52b0b410e36d48c9b02858daa034cc96
Venice Vargas <venicevargas1@live.com>
2020-09-19 08:11:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,15 +17,9 @@ class Song
     song
   end
 
-  #this is a setter method, a writer,
-  #it sets the artist attribute of the song instance equal to an artist instance (rather than just a string)
-  #it calls a Class Method in Artist, that returns an artist object
-  #the find_or_create_by_name either finds an existing artist and returns it, or creates a new object and returns it
+ 
   def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    #after the artist instance is returned
-    #this method calls the artist instance method #add_song
-    #add_song adds this current song instance to the artist's song list
+    self.artist = Artist
     self.artist.add_song(self)
   end
 
