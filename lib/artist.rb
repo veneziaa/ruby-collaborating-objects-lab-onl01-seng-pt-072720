1c61c93d9457c900f7b5a3ccf6b0f0b92ddf3ae0
Venice Vargas <venicevargas1@live.com>
2020-09-19 08:15:55 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,86 +1,39 @@
-class Artist 
+class Artist
 
-	attr_accessor :name 
-	@@all = []
+  attr_accessor :name, :songs
+  @@all =[]
 
-	def initialize(name)
-		@name = name
-		@songs = []
-	end 
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
 
-	def save
-		@@all << self 
-	end 
+  def add_song(song)
+    @songs << song
+  end
 
-	def songs
-		@songs
-	end 
+  def save
+    @@all << self #saves the artist name to the all array
+  end
 
-	def add_song(song)
-		@songs << song 
-	end
+  def self.all
+    @@all #displays the all array
+  end
 
-	def self.find_or_create_by_name(name)
-		@@all.each  do |artist|
-			if artist.name == name 
-				return artist 
-			else 
-				return self.new(name)
-			end 
-		end 
-	end 
+  def self.find_or_create_by_name(artist_name)
+    found_artist = self.all.find {|artist| artist.name == artist_name}
+    if found_artist
+      found_artist
+    else
+      new_artist = self.new(artist_name)
+      new_artist.save
+      new_artist
+    end
+  end
 
-	def self.all 
-		@@all 
-	end 
-
-
-end 
-
-
-
-
-class Artist 
-
-	attr_accessor :name 
-	@@all = []
-
-	def initialize(name)
-		@name = name
-		@songs = []
-	end 
-
-	def save
-		@@all << self 
-	end 
-
-	def songs
-		@songs
-	end 
-
-	def add_song(song)
-		@songs << song 
-	end
-
-	def self.find_or_create_by_name(name)
-		@@all.each  do |artist|
-			if artist.name == name 
-				return artist 
-			else 
-				return self.new(name)
-			end 
-		end 
-	end 
-
-	def self.all 
-		@@all 
-	end 
-
-	def print_songs 
-		@songs.each do |song|
-			puts song.name
-		end 
-	end
-
-
-end 
+  def print_songs
+    @songs.each do |song|
+      puts song.name
+    end
+  end
+end
